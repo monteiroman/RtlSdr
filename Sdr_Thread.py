f894d58bb968bc9b7d8ade7d755e7a83408ee219
Tiago <monteirotiagom90@gmail.com>
2019-04-08 18:25:29 -0300

    FM reciever with FFT graph

--- Sdr_Thread.py
+++ Sdr_Thread.py
@@ -18,13 +18,15 @@ from pylab import *
 from rtlsdr import *
 
 import threading
+import numpy as np
+
 
 style.use("ggplot")
 
 # f = Figure(figsize=(7,3), dpi=78)
 # a = f.add_subplot(111)
 
-ANIM_INTVL = 1000        # Animation Interval in mS
+ANIM_INTVL = 50        # Animation Interval in mS
 
 
 
@@ -42,10 +44,42 @@ class SensorThread(threading.Thread):
 
     def run(self):
         while True:
-            self.dataStruct.setSamples(self.sdr.read_samples(256*1024))
-            # time.sleep(0.05)
+            aux = fix_iq_imbalance(self.sdr.read_samples(256*1024))
+            self.dataStruct.setSamples(aux)
+            time.sleep(0.1)
             if self.dataStruct.getQuitState():
                 break
+
+
+
+
+
+def fix_iq_imbalance(x):
+    # remove DC and save input power
+    z = x - np.mean(x)
+    p_in = np.var(z)
+
+    # scale Q to have unit amplitude (remember we're assuming a single input tone)
+    Q_amp = np.sqrt(2 * np.mean(x.imag**2))
+    z /= Q_amp
+
+    I, Q = z.real, z.imag
+
+    alpha_est = np.sqrt(2 * np.mean(I**2))
+    sin_phi_est = (2 / alpha_est) * np.mean(I * Q)
+    cos_phi_est = np.sqrt(1 - sin_phi_est**2)
+
+    I_new_p = (1 / alpha_est) * I
+    Q_new_p = (-sin_phi_est / alpha_est) * I + Q
+
+    y = (I_new_p + 1j * Q_new_p) / cos_phi_est
+
+    print('phase error:', np.arccos(cos_phi_est) * 360 / 2 / np.pi, 'degrees')
+    print('amplitude error:', 20 * np.log10(alpha_est), 'dB')
+
+    return y * np.sqrt(p_in / np.var(y))
+
+
 
 
 
@@ -55,7 +89,7 @@ def main():
 
     mythread = SensorThread(name = "SdrThread")
     mythread.setDataStruct(dataStruct)
-    mythread.setSDR(3e6, 98.5e6, 10)
+    mythread.setSDR(3e6, 300e6, 20)
     mythread.start()
 
     app = BMeasureApp(dataStruct=dataStruct)
@@ -64,7 +98,7 @@ def main():
 
     while True:
         winUpdate (app)
-        time.sleep(0.05)
+        # time.sleep(0.05)
 
 if __name__ == '__main__':
     main()
